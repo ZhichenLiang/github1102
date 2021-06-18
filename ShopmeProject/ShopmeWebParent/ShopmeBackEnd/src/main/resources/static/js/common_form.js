889cc5c54409f761047ae061908726c8a9d3e53b
Zhichen Liang <zhichennr@gmail.com>
2021-06-18 14:34:26 +0800

    Code create Category function

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form.js
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form.js
@@ -6,8 +6,8 @@ $(document).ready(function() {
 	$("#fileImage").change(function() {
 		fileSize = this.files[0].size;
 
-		if (fileSize > 1048576) {
-			this.setCustomValidity("You must choose an image less than 1 MB! ");
+		if (fileSize > 102400) {
+			this.setCustomValidity("You must choose an image less than 100 KB! ");
 			this.reportValidity;
 		} else {
 			this.setCustomValidity("");
