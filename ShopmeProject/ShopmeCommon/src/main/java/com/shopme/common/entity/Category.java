0c1548dfb0925c52b35df998d8a3cd3514a7b885
Zhichen Liang <zhichennr@gmail.com>
2021-06-23 14:30:56 +0800

    Check Uniqueness of Category

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
@@ -95,6 +95,15 @@ public class Category {
 		this(name);
 		this.parent = parent;
 	}
+	
+	
+
+	public Category(Integer id, String name, String alias) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.alias = alias;
+	}
 
 	public Integer getId() {
 		return id;
