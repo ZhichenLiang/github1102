724216aad4d900a812f83f451edde727bfa25eaf
Zhichen Liang <80897467+ZhichenLiang@users.noreply.github.com>
2021-07-05 22:25:46 +0800

    Code brand form

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Brand.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Brand.java
@@ -12,6 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "brands")
@@ -28,21 +29,21 @@ public class Brand {
 	
 	@ManyToMany
 	@JoinTable(
-			name = "brands_categories", 
+			name = "brands_categories",
 			joinColumns = @JoinColumn(name = "brand_id"),
 			inverseJoinColumns = @JoinColumn(name = "category_id")
 			)
 	private Set<Category> categories = new HashSet<>();
 
 	public Brand() {
-
- 	}
-
- 	public Brand(String name) {
- 		this.name = name;
- 		this.logo = "brand-logo.png";
- 	}	
+		
+	}
 	
+	public Brand(String name) {
+		this.name = name;
+		this.logo = "brand-logo.png";
+	}
+
 	public Integer getId() {
 		return id;
 	}
@@ -75,9 +76,15 @@ public class Brand {
 		this.categories = categories;
 	}
 
- 	@Override
- 	public String toString() {
- 		return "Brand [id=" + id + ", name=" + name + ", categories=" + categories + "]";
- 	}
-	
+	@Override
+	public String toString() {
+		return "Brand [id=" + id + ", name=" + name + ", categories=" + categories + "]";
+	}
+
+	@Transient
+	public String getLogoPath() {
+		if (this.id == null) return "/images/image-thumbnail.png";
+		
+		return "/brand-logos/" + this.id + "/" + this.logo;		
+	}
 }
--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandController.java
@@ -1,25 +1,41 @@
 package com.shopme.admin.brand;
 
- import java.util.List;
+import java.util.List;
 
- import org.springframework.beans.factory.annotation.Autowired;
- import org.springframework.stereotype.Controller;
- import org.springframework.ui.Model;
- import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
- import com.shopme.common.entity.Brand;
+import com.shopme.admin.category.CategoryService;
+import com.shopme.common.entity.Brand;
+import com.shopme.common.entity.Category;
 
- @Controller
- public class BrandController {
+@Controller
+public class BrandController {
 
- 	@Autowired
- 	private BrandService service;
-
- 	@GetMapping("/brands")
- 	public String listAll(Model model) {
- 		List<Brand> listBrands = service.listAll();
- 		model.addAttribute("listBrands", listBrands);
-
- 		return "brands/brands";
- 	}
- }
+	@Autowired
+	private BrandService brandService;
+	
+	@Autowired
+	private CategoryService categoryService;
+	
+	@GetMapping("/brands")
+	public String listAll(Model model) {
+		List<Brand> listBrands = brandService.listAll();
+		model.addAttribute("listBrands", listBrands);
+		
+		return "brands/brands";
+	}
+	
+	@GetMapping("/brands/new")
+	public String newBrand(Model model) {
+		List<Category> listCategories = categoryService.listCategoriesUsedInForm();
+		
+		model.addAttribute("listCategories", listCategories);
+		model.addAttribute("brand", new Brand());
+		model.addAttribute("pageTitle", "Create New Brand");
+		
+		return "brands/brand_form";		
+	}
+}
