bb7a9bfa37d3fa6bdae19e63c19072f6978aed75
Zhichen Liang <80897467+ZhichenLiang@users.noreply.github.com>
2021-07-09 22:18:17 +0800

    Code check uniquess of product

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductService.java
@@ -1,6 +1,7 @@
 package com.shopme.admin.product;
 
- import java.util.List;
+ import java.util.Date;
+import java.util.List;
 
  import org.springframework.beans.factory.annotation.Autowired;
  import org.springframework.stereotype.Service;
@@ -14,5 +15,36 @@ package com.shopme.admin.product;
 
  	public List<Product> listAll() {
  		return (List<Product>) repo.findAll();
+ 	}
+ 	
+ 	public Product save(Product product) {
+ 		if (product.getId() == null) {
+ 			product.setCreatedTime(new Date());
+ 		}
+
+ 		if (product.getAlias() == null || product.getAlias().isEmpty()) {
+ 			String defaultAlias = product.getName().replaceAll(" ", "-");
+ 			product.setAlias(defaultAlias);
+ 		} else {
+ 			product.setAlias(product.getAlias().replaceAll(" ", "-"));
+ 		}
+
+ 		product.setUpdatedTime(new Date());
+
+ 		return repo.save(product);
+ 	}
+ 	public String checkUnique(Integer id, String name) {
+ 		boolean isCreatingNew = (id == null || id == 0);
+ 		Product productByName = repo.findByName(name);
+
+ 		if (isCreatingNew) {
+ 			if (productByName != null) return "Duplicate";
+ 		} else {
+ 			if (productByName != null && productByName.getId() != id) {
+ 				return "Duplicate";
+ 			}
+ 		}
+
+ 		return "OK";
  	}
  }
