699f5947565b095e82889ea5c590fce233c8fd5a
Zhichen Liang <80897467+ZhichenLiang@users.noreply.github.com>
2021-07-09 17:59:15 +0800

    Code Unit Tests for Product Repository

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
@@ -204,5 +204,10 @@ public class Product {
 	public void setBrand(Brand brand) {
 		this.brand = brand;
 	}
+	
+	@Override
+ 	public String toString() {
+ 		return "Product [id=" + id + ", name=" + name + "]";
+ 	}
 
 }
