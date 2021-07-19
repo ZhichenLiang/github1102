de087e8558641c91458806684c6d70682ba61909
Zhichen Liang <80897467+ZhichenLiang@users.noreply.github.com>
2021-07-19 23:14:23 +0800

    Code product details page

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
@@ -11,6 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 
 import org.springframework.data.annotation.Transient;
@@ -42,6 +43,7 @@ public class Category {
 	private Category parent;
 	
 	@OneToMany(mappedBy= "parent")
+	@OrderBy("name asc")
 	private Set<Category> children = new HashSet<>();
 	
 	
