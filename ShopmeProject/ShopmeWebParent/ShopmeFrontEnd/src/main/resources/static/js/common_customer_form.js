eccd89c209730418855bc5855ddb8ab25cf1e582
Zhichen Liang <80897467+ZhichenLiang@users.noreply.github.com>
2021-07-28 15:21:13 +0800

    Code quantity control component

--- ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/resources/static/js/common_customer_form.js
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/resources/static/js/common_customer_form.js
@@ -38,16 +38,3 @@ var dropDownCountry;
  	}
  }
 
- function showModalDialog(title, message) {
- 	$("#modalTitle").text(title);
- 	$("#modalBody").text(message);
- 	$("#modalDialog").modal();
- }
-
- function showErrorModal(message) {
- 	showModalDialog("Error", message);
- }
-
- function showWarningModal(message) {
- 	showModalDialog("Warning", message);
- }	
