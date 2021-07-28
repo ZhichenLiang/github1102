77e15d0c91dea5d748b3474dc94c3fc027d4b022
Zhichen Liang <80897467+ZhichenLiang@users.noreply.github.com>
2021-07-28 23:56:05 +0800

    Code update product quantity in shopping cart

--- ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartService.java
@@ -1,48 +1,61 @@
 package com.shopme.shoppingcart;
 
- import java.util.List;
+import java.util.List;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
- import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+import com.shopme.common.entity.CartItem;
+import com.shopme.common.entity.Customer;
+import com.shopme.common.entity.Product;
+import com.shopme.product.ProductRepository;
 
- import com.shopme.common.entity.CartItem;
- import com.shopme.common.entity.Customer;
- import com.shopme.common.entity.Product;
+@Service
+@Transactional
+public class ShoppingCartService {
 
- @Service
- public class ShoppingCartService {
+	@Autowired
+	private CartItemRepository cartRepo;
+	@Autowired
+	private ProductRepository productRepo;
 
- 	@Autowired private CartItemRepository cartRepo;
+	public Integer addProduct(Integer productId, Integer quantity, Customer customer) throws ShoppingCartException {
+		Integer updatedQuantity = quantity;
+		Product product = new Product(productId);
 
- 	public Integer addProduct(Integer productId, Integer quantity, Customer customer) 
- 			throws ShoppingCartException {
- 		Integer updatedQuantity = quantity;
- 		Product product = new Product(productId);
+		CartItem cartItem = cartRepo.findByCustomerAndProduct(customer, product);
 
- 		CartItem cartItem = cartRepo.findByCustomerAndProduct(customer, product);
+		if (cartItem != null) {
+			updatedQuantity = cartItem.getQuantity() + quantity;
 
- 		if (cartItem != null) {
- 			updatedQuantity = cartItem.getQuantity() + quantity;
+			if (updatedQuantity > 5) {
+				throw new ShoppingCartException("Could not add more " + quantity + " item(s)"
+						+ " because there's already " + cartItem.getQuantity() + " item(s) "
+						+ "in your shopping cart. Maximum allowed quantity is 5.");
+			}
+		} else {
+			cartItem = new CartItem();
+			cartItem.setCustomer(customer);
+			cartItem.setProduct(product);
+		}
 
- 			if (updatedQuantity > 5) {
- 				throw new ShoppingCartException("Could not add more " + quantity + " item(s)"
- 						+ " because there's already " + cartItem.getQuantity() + " item(s) "
- 						+ "in your shopping cart. Maximum allowed quantity is 5.");
- 			}
- 		} else {
- 			cartItem = new CartItem();
- 			cartItem.setCustomer(customer);
- 			cartItem.setProduct(product);
- 		}
+		cartItem.setQuantity(updatedQuantity);
 
- 		cartItem.setQuantity(updatedQuantity);
+		cartRepo.save(cartItem);
 
- 		cartRepo.save(cartItem);
-
- 		return updatedQuantity;
- 	}
+		return updatedQuantity;
+	}
 
 	public List<CartItem> listCartItems(Customer customer) {
 		return cartRepo.findByCustomer(customer);
 	}
- }
+
+	public float updateQuantity(Integer productId, Integer quantity, Customer customer) {
+		cartRepo.updateQuantity(quantity, customer.getId(), productId);
+		Product product = productRepo.findById(productId).get();
+		float subtotal = product.getDiscountPrice() * quantity;
+		return subtotal;
+	}
+}
